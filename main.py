e6410aeea4b9e782959230828361080ec36d9045
Quantum8060 <akaunnt8060@gmail.com>
2024-08-05 00:54:16 +0900

    announceコマンド改造失敗

--- main.py
+++ main.py
@@ -92,24 +92,6 @@ async def save_company_data(data):
 
 
 
-announce_file = 'announce.json'
-
-async def load_announce_data():
-    try:
-        async with aiofiles.open(announce_file, 'r') as a_file:
-            return json.loads(await a_file.read())
-    except FileNotFoundError:
-        return {}
-
-async def save_announce_data(a_data):
-    async with aiofiles.open(announce_file, 'w') as a_file:
-        await a_file.write(json.dumps(a_data, indent=4))
-
-
-
-
-
-
 conn = sqlite3.connect('users.db')
 c = conn.cursor()
 
@@ -857,7 +839,6 @@ cogs_list = [
     'stop',
     'tasks',
     'report',
-    'announce',
 ]
 
 for cog in cogs_list:
