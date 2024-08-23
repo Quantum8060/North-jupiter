6f59aa7df478e3f1d30d05af1046365905af1eb2
Quantum8060 <akaunnt8060@gmail.com>
2024-08-24 00:53:12 +0900

    ブラックリストの保存場所を更新

--- cogs/blacklist.py
+++ cogs/blacklist.py
@@ -3,19 +3,40 @@ from discord.ext import commands
 from discord.commands import SlashCommandGroup
 from discord.ext.commands import NotOwner
 import json
+import sqlite3
+from discord.ext.pages import Paginator, Page
 
 Debug_guild = [1235247721934360577]
 main_guild = [962647934695002173, 1235247721934360577, 1256021750756544632]
 
-blacklist_file = 'blacklist.json'
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
 
-def load_blacklist_data():
-    with open(blacklist_file, 'r') as file:
-        return json.load(file)
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (id TEXT PRIMARY KEY, cash INTEGER )''')
+c.execute('''CREATE TABLE IF NOT EXISTS blacklist
+             (id TEXT PRIMARY KEY, cash INTEGER)''')
 
-def save_blacklist_data(data):
-    with open(blacklist_file, 'w') as file:
-        json.dump(data, file, indent=4)
+conn.commit()
+
+
+def get_user_info(user_id):
+    c.execute("SELECT id, cash FROM users WHERE id = ?", (user_id,))
+    return c.fetchone()
+
+def save_blacklist(blacklist_id, cash):
+    with conn:
+        c.execute("INSERT OR IGNORE INTO blacklist (id, cash) VALUES (?, ?)", (blacklist_id, cash))
+        c.execute("UPDATE blacklist SET cash = ? WHERE id = ?", (cash, blacklist_id))
+
+def get_all_blacklist():
+    c.execute("SELECT id, cash FROM blacklist")
+    return c.fetchall()
+
+def get_blacklist_info(blacklist_id):
+    c.execute("SELECT id, cash FROM blacklist WHERE id = ?", (blacklist_id,))
+    return c.fetchone()
+
 
 
 class blacklist(commands.Cog):
@@ -28,21 +49,21 @@ class blacklist(commands.Cog):
 
     @blacklists.command(name="add", description="ユーザーをブラックリストに追加します。", guild_ids=main_guild)
     @commands.is_owner()
-    async def a_blacklist(self, interaction: discord.ApplicationContext, user:discord.Member, reason: discord.Option(str, description="理由を入力します。")):
+    async def a_blacklist(self, interaction: discord.ApplicationContext, user:discord.Member):
 
-        data = load_blacklist_data()
+        user_id = str(user.id)
+        user_info = get_blacklist_info(user_id)
+        user_bal = get_user_info(user.id)
 
         user_id = await self.bot.fetch_user(f"{user.id}")
 
-        data = load_blacklist_data()
-
-        if user_id not in data:
-            await interaction.respond(f"{user.mention}をブラックリストに追加しました。", ephemeral=True)
-
-            data[str(user.id)] = reason
-            save_blacklist_data(data)
-        else:
+        if user_info:
             await interaction.response.send_message("このユーザーはすでにブラックリストに追加されています。", ephemeral=True)
+        else:
+            user_id = str(user.id)
+            cash = int(user_bal[1])
+            save_blacklist(user_id, cash)
+            await interaction.respond(f"{user.mention}をブラックリストに追加しました。", ephemeral=True)
 
     @a_blacklist.error
     async def adderror(ctx, error):
@@ -56,25 +77,23 @@ class blacklist(commands.Cog):
 
     @blacklists.command(name="show", description="ブラックリストを一覧表示します。", guild_ids=main_guild)
     @commands.is_owner()
-    async def s_blacklist(self, interaction: discord.ApplicationContext):
+    async def s_blacklist(self, ctx: discord.ApplicationContext):
 
-        data = load_blacklist_data()
+        blacklist = get_all_blacklist()
 
-        try:
-            with open(blacklist_file, 'r') as file:
-                data = json.load(file)
-        except FileNotFoundError:
-            await interaction.send("データファイルが見つかりませんでした。")
+        if not blacklist:
+            await ctx.respond("ブラックリストにユーザーが登録されていません。", ephemeral=True)
             return
 
-        data = load_blacklist_data()
+        embeds = []
+        for i in range(0, len(blacklist), 5):
+            embed = discord.Embed(title="ブラックリスト", color=0x00ff00)
+            for blacklist_id, cash in blacklist[i:i+5]:
+                embed.add_field(name=f"ユーザー: {blacklist_id}", value=f"所持金: {cash}", inline=False)
+            embeds.append(embed)
 
-        embed = discord.Embed(title="ブラックリストユーザー一覧")
-
-        user_info = "\n".join([f"<@!{key}> : {value}" for key, value in data.items()])
-        embed.add_field(name="ブラックリストユーザーの一覧です。", value=user_info, inline=False)
-
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        paginator = Paginator(pages=embeds, use_default_buttons=True, timeout=60)
+        await paginator.respond(ctx.interaction, ephemeral=True)
 
     @s_blacklist.error
     async def showerror(ctx, error):
@@ -90,13 +109,16 @@ class blacklist(commands.Cog):
     @commands.is_owner()
     async def r_blacklist(self, interaction: discord.ApplicationContext, user: discord.Member):
 
-        data = load_blacklist_data()
+        user_id = str(user.id)
+        user_info = get_blacklist_info(user_id)
 
         user_id = str(user.id)
-        if user_id in data:
-            del data[user_id]
-            save_blacklist_data(data)
+        if user_info:
+            c.execute(f"""DELETE FROM blacklist WHERE id="{user_id}";""")
+            conn.commit()
+
             await interaction.response.send_message(f"{user.mention}をブラックリストから削除しました。", ephemeral=True)
+
         else:
             await interaction.response.send_message("このユーザーはブラックリストに登録されていません。", ephemeral=True)
 
